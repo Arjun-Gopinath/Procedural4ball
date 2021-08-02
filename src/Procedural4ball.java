91c6227bb6e5c9478f1cac0adeb083ae920c4d5d
Arjun-Gopinath <arjungopinath06@gmail.com>
2021-08-02 20:05:36 +0530

    removed code smells

--- src/Procedural4ball.java
+++ src/Procedural4ball.java
@@ -5,7 +5,7 @@ public class Procedural4ball extends PApplet {
     public static final int HEIGHT = 480;
     public static final int WIDTH = 640;
     public static final int DIAMETER = 10;
-    public static int ball = 0;
+    public static int position = 0;
 
     @Override
     public void settings() {
@@ -15,15 +15,14 @@ public class Procedural4ball extends PApplet {
 
     @Override
     public void draw() {
-        moveBall(ball,HEIGHT/5);
-        moveBall(2*ball,2*HEIGHT/5);
-        moveBall(3*ball,3*HEIGHT/5);
-        moveBall(4*ball,4*HEIGHT/5);
-        ball+=1;
+        for (int speed = 1; speed <= 4; speed++) {
+            ball(speed*position,speed*HEIGHT/5);
+        }
+        position +=1;
     }
 
-    private void moveBall(int movementX, int height) {
-        ellipse(movementX,height,DIAMETER, DIAMETER);
+    private void ball(int positionX, int height) {
+        ellipse(positionX,height,DIAMETER, DIAMETER);
     }
     
     public static void main(String[] args) {
